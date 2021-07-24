20dd60bba7d97f630c353360eec01173a0ad56eb
uzayyildiztaskan <55345785+uzayyildiztaskan@users.noreply.github.com>
2021-07-24 19:00:08 +0300

    Add files via upload

--- createDatabase.js
+++ createDatabase.js
@@ -1,5 +1,6 @@
 const { Console } = require('console');
 var mysql = require('mysql');
+const { getSystemErrorMap } = require('util');
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -26,4 +27,18 @@ con.connect(function(err) {
     if(err) console.log("Table bireysel_musteri already exists.");
     else console.log("Table bireysel_musteri Created");
   });
+
+  con.query("CREATE TABLE personel(personel_no INT PRIMARY KEY, yonetici_no INT NOT NULL, tesis_no INT NOT NULL, kimlik_no INT NOT NULL, isim VARCHAR(20) NOT NULL, soyisim VARCHAR(20) NOT NULL, dogum_tarihi DATE NOT NULL, yas INT NOT NULL, telefon_no VARCHAR(15) NOT NULL, adres VARCHAR(100) NOT NULL, mail_adresi VARCHAR(30) NOT NULL, kan_grubu VARCHAR(10) NOT NULL, maas INT NOT NULL, gorev VARCHAR(30) NOT NULL, departman VARCHAR(30) NOT NULL, calisma_saati_baslangic VARCHAR(5) NOT NULL, calisma_saati_bitis VARCHAR(5) NOT NULL, izin_gunleri VARCHAR(200))", function(err, result) {
+
+    if(err) console.log("Table personel already exists.");
+    else console.log("Table personel Created");
+  });
+
+  con.query("CREATE TABLE oda(oda_no INT PRIMARY KEY, rezervasyon_no INT NOT NULL, yatak_sayisi INT NOT NULL, oda_tipi VARCHAR(20) NOT NULL, fiyat INT NOT NULL, kat INT NOT NULL, cephe VARCHAR(10) NOT NULL)", function(err, result) {
+
+    if(err) console.log("Table oda already exists.");
+    else console.log("Table oda Created");
+  });
 });
+
+
