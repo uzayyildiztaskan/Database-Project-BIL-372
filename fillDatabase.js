0f0904b58fe7a41ebe2d7d80d63f299f0ced4a26
BerkKaraaslan <71693771+BerkKaraaslan@users.noreply.github.com>
2021-07-27 19:03:29 +0300

    Personel, oda ve aktiviteye datalar eklendi.

--- fillDatabase.js
+++ fillDatabase.js
@@ -104,80 +104,157 @@ con.connect(function(err) {
         console.log("Number of records inserted: " + result.affectedRows);
     });
 
-    sql = "INSERT INTO rezervasyon(rezervasyon_no, musteri_no, oda_sayisi, kisi_sayisi, baslangic_tarihi, bitis_tarihi, fiyat) VALUES ?";
+
+
+    sql = "INSERT INTO personel(personel_no, yonetici_no, tesis_no, kimlik_no, isim, soyisim, dogum_tarihi, yas, telefon_no, adres, mail_adresi, kan_grubu, maas, gorev, departman, calisma_saati_baslangic, calisma_saati_bitis, izin_gunleri) VALUES ?";
     values = [
-        [543, null, 3, 1, '1982/12/2', '1982/12/8', 3690],
-        [532, null, 2, 5, '2007/9/15', '2007/9/18', 2556],
-        [182, null, 3, 4, '2000/3/5', '2000/3/13', 2139],
-        [439, null, 1, 5, '2012/5/3', '2012/5/5', 521],
-        [503, null, 1, 5, '2009/8/16', '2009/8/22', 870],
-        [353, null, 1, 1, '2005/6/11', '2005/6/15', 1441],
-        [433, null, 2, 2, '2004/2/3', '2004/2/8', 2470],
-        [79, null, 3, 3, '2008/7/8', '2008/7/15', 4143],
-        [177, null, 1, 3, '2015/10/13', '2015/10/19', 1160],
-        [53, null, 3, 4, '2018/2/22', '2018/2/25', 3318],
-        [997, null, 3, 5, '1983/10/30', '1983/11/5', 2985],
-        [502, null, 3, 3, '1983/10/12', '1983/10/18', 4272],
-        [782, null, 2, 7, '2013/5/28', '2013/6/3', 1578],
-        [440, null, 1, 6, '1983/12/21', '1983/12/26', 814],
-        [236, null, 1, 6, '2019/5/20', '2019/5/28', 543],
-        [179, null, 3, 4, '2010/1/17', '2010/1/19', 2787],
-        [379, null, 2, 4, '1986/3/8', '1986/3/15', 2852],
-        [381, null, 2, 4, '1995/5/27', '1995/6/1', 2428],
-        [936, null, 2, 3, '2020/11/12', '2020/11/14', 1920],
-        [407, null, 3, 1, '1984/10/10', '1984/10/13', 3801],
-        [278, null, 2, 4, '1980/8/10', '1980/8/13', 2362],
-        [469, null, 2, 7, '1993/12/30', '1994/1/7', 2870],
-        [411, null, 3, 7, '1990/8/15', '1990/8/20', 3876],
-        [89, null, 3, 3, '2009/1/22', '2009/1/29', 3456],
-        [519, null, 1, 3, '1990/12/10', '1990/12/17', 1310],
-        [347, null, 2, 6, '1985/3/30', '1985/3/7', 1020],
-        [553, null, 2, 6, '2010/2/16', '2010/2/20', 2384],
-        [438, null, 2, 2, '2000/2/14', '2000/2/18', 1144],
-        [377, null, 2, 1, '2018/6/27', '2018/7/4', 1912],
-        [27, null, 2, 1, '1993/10/4', '1993/10/7', 1850],
-        [958, null, 1, 2, '1993/11/10', '1993/11/12', 1290],
-        [907, null, 3, 6, '1988/3/7', '1988/3/14', 2892],
-        [115, null, 2, 3, '1985/7/5', '1985/7/7', 2280],
-        [456, null, 2, 4, '1981/11/22', '1981/11/29', 2846],
-        [310, null, 2, 2, '1989/8/11', '1989/8/18', 1608]
-    ];
+        [0, null, null, 7291253228, 'Harley', 'Briggs', '23/9/1958', 62, '13108165429', '4 Old Colony Way Yarmouth MA 02664', 'Harley@gmail.com', '0 rh-', 7500, 'Hizmet Personeli', 'Spa', '20.00', '08.00', 'Cumartesi-Pazar'],
+        [1, null, null, 5312646933, 'Connor', 'Briggs', '26/2/1972', 48, '19128884766', '310 Timrod Road Manchester CT 06040', 'Connor@gmail.com', 'AB rh-', 10000, 'Yönetici', 'Disco', '20.00', '08.00', 'Pazartesi-Cuma'],
+        [2, null, null, 1106489528, 'Amir', 'Shelton', '2/9/1971', 49, '17175209890', '8821 West Myrtle Avenue Glendale AZ 85305', 'Amir@gmail.com', '0 rh-', 7500, 'Sekreter', 'Spa', '20.00', '08.00', 'Pazartesi-Perşembe'],
+        [3, null, null, 5802774145, 'Ellie', 'Edwards', '27/2/1944', 76, '19160455660', '5108 Franklin Street Savannah GA 31405', 'Ellie@gmail.com', 'B rh+', 10000, 'Sekreter', 'Disco', '20.00', '08.00', 'Pazartesi-Cuma'],
+        [4, null, null, 2969726746, 'Kieron', 'Hunt', '24/10/1952', 68, '15167618280', '8376 Albacore Drive Pasadena MD 21122', 'Kieron@gmail.com', 'AB rh+', 2800, 'Sekreter', 'Spor Salonu', '08.00', '20.00', 'Cumartesi-Pazar'],
+        [5, null, null, 0927825671, 'Farhan', 'Moreno', '27/4/1949', 71, '19161959041', '1770 Colony Way Fayetteville AR 72704', 'Farhan@gmail.com', 'AB rh-', 2800, 'Sekreter', 'Golf', '09.00', '17.00', 'Pazar-Pazartesi'],
+        [6, null, null, 6166265921, 'Yasin', 'Meyer', '7/12/2006', 14, '19178909397', '109 Summit Street Burlington VT 05401', 'Yasin@gmail.com', '0 rh+', 10000, 'Garson', 'Havuz', '09.00', '17.00', 'Pazartesi-Cuma'],
+        [7, null, null, 5605889154, 'Zakariya', 'Meyer', '1/1/1976', 44, '17152343579', '165 Saint John Street Manchester CT 06040', 'Zakariya@gmail.com', 'AB rh-', 7500, 'Sekreter', 'Spa', '08.00', '20.00', 'Pazar-Pazartesi'],
+        [8, null, null, 5048473335, 'Kieron', 'Reid', '12/5/1942', 78, '13198280368', '5114 Greentree Drive Nashville TN 37211', 'Kieron@gmail.com', 'A rh-', 5000, 'Yönetici', 'Disco', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [9, null, null, 6970034405, 'Roman', 'Mccoy', '9/1/1979', 41, '18176849148', '132 Laurel Green Court Savannah GA 31419', 'Roman@gmail.com', 'AB rh+', 10000, 'Hizmet Personeli', 'İdari', '09.00', '17.00', 'Pazartesi-Cuma'],
+        [10, null, null, 8321518159, 'Tristan', 'Carrillo1', '9/11/1991', 29, '15160775515', '5811 Crossings Boulevard Nashville TN 37013', 'Tristan@gmail.com', 'B rh-', 10000, 'Yönetici', 'İdari', '08.00', '20.00', 'Cumartesi-Pazar'],
+        [11, null, null, 6985139330, 'Ellie', 'Phillips', '4/7/1995', 25, '15126686680', '519 West 75th Avenue #APT 000003 Anchorage AK 99518', 'Ellie@gmail.com', 'B rh+', 5000, 'Hizmet Personeli', 'Mutfak', '20.00', '08.00', 'Pazar-Pazartesi'],
+        [12, null, null, 1168077233, 'Jimmy', 'White', '27/6/1969', 51, '19126123165', '1407 Walden Court Crofton MD 21114', 'Jimmy@gmail.com', 'B rh+', 5000, 'Muhasebeci', 'Disco', '09.00', '17.00', 'Cumartesi-Pazar'],
+        [13, null, null, 7487528953, 'Zakariya', 'Rodriguez', '5/1/1940', 80, '19160028698', '210 Lacross Lane Westmore VT 05860', 'Zakariya@gmail.com', 'A rh-', 10000, 'Sekreter', 'Spa', '20.00', '08.00', 'Cumartesi-Pazar'],
+        [14, null, null, 6431864495, 'Anabelle', 'Mullins', '25/9/1993', 27, '18190245594', '150 Carter Street Manchester CT 06040', 'Anabelle@gmail.com', '0 rh+', 3500, 'Hizmet Personeli', 'Spa', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [15, null, null, 5090093644, 'Allen', 'Mccoy', '22/6/1955', 65, '18167980382', '81 Seaton Place Northwest Washington DC 20001', 'Allen@gmail.com', '0 rh+', 10000, 'Yönetici', 'Spor Salonu', '20.00', '08.00', 'Pazartesi-Cuma'],
+        [16, null, null, 2525078980, 'Charles', 'Mccarthy', '27/4/1940', 80, '18178033897', '1364 Capri Drive Panama City FL 32405', 'Charles@gmail.com', '0 rh-', 2800, 'Hizmet Personeli', 'Disco', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [17, null, null, 2941881759, 'Allanah', 'Carrillo1', '16/7/2010', 10, '19180437403', '130 Old Route 103 Chester VT 05143', 'Allanah@gmail.com', '0 rh-', 10000, 'Sekreter', 'Disco', '20.00', '08.00', 'Pazartesi-Cuma'],
+        [18, null, null, 2662866012, 'Antonia', 'Mccoy', '13/7/1995', 25, '19117444795', '1508 Massachusetts Avenue Southeast Washington DC 20003', 'Antonia@gmail.com', 'A rh-', 10000, 'Hizmet Personeli', 'Golf', '08.00', '20.00', 'Cumartesi-Pazar'],
+        [19, null, null, 6728937564, 'Connor', 'Griffin', '11/5/1949', 71, '19163533345', '8800 Cordell Circle #APT 000003 Anchorage AK 99502', 'Connor@gmail.com', 'B rh+', 3500, 'Garson', 'İdari', '09.00', '17.00', 'Cumartesi-Pazar'],
+        [20, null, null, 4278891262, 'Allen', 'Griffin', '21/10/1968', 52, '19125292925', '5461 West Shades Valley Drive Montgomery AL 36108', 'Allen@gmail.com', 'B rh+', 5000, 'Sekreter', 'Havuz', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [21, null, null, 9921658691, 'Charlotte', 'Shelton', '23/7/1984', 36, '15184552956', '4 Old Colony Way Yarmouth MA 02664', 'Charlotte@gmail.com', 'A rh-', 10000, 'Garson', 'Mutfak', '20.00', '08.00', 'Pazartesi-Perşembe'],
+        [22, null, null, 3621798089, 'Caitlyn', 'Haynes', '5/8/2008', 12, '17152190173', '74 Ranch Drive Montgomery AL 36109', 'Caitlyn@gmail.com', 'AB rh-', 5000, 'Hizmet Personeli', 'İdari', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [23, null, null, 8267606466, 'Allanah', 'Olson', '11/6/1951', 69, '15171991394', '18 Densmore Drive Essex VT 05452', 'Allanah@gmail.com', 'A rh+', 2800, 'Yönetici', 'Golf', '20.00', '08.00', 'Cumartesi-Pazar'],
+        [24, null, null, 6349573165, 'Leonard', 'Laing', '17/3/1993', 27, '13106770779', '8821 West Myrtle Avenue Glendale AZ 85305', 'Leonard@gmail.com', '0 rh+', 10000, 'Muhasebeci', 'Disco', '08.00', '20.00', 'Cumartesi-Pazar'],
+        [25, null, null, 2259988038, 'Farhan', 'Carrillo1', '16/10/1975', 45, '15161565664', '232 Maine Avenue Panama City FL 32401', 'Farhan@gmail.com', 'B rh+', 5000, 'Hizmet Personeli', 'Mutfak', '20.00', '08.00', 'Pazartesi-Perşembe'],
+        [26, null, null, 3687164176, 'Amir', 'Fernandez', '15/10/2019', 1, '19162422359', '210 Lacross Lane Westmore VT 05860', 'Amir@gmail.com', '0 rh-', 7500, 'Yönetici', 'Golf', '09.00', '17.00', 'Pazartesi-Perşembe'],
+        [27, null, null, 7540470286, 'Kieron', 'White', '3/12/1972', 48, '19125561305', '913 Fallview Trail Nashville TN 37211', 'Kieron@gmail.com', '0 rh-', 3500, 'Sekreter', 'Spor Salonu', '08.00', '20.00', 'Pazartesi-Cuma'],
+        [28, null, null, 6111845998, 'Kieron', 'Shelton', '13/6/1990', 30, '17108941379', '1508 Massachusetts Avenue Southeast Washington DC 20003', 'Kieron@gmail.com', 'A rh-', 2800, 'Hizmet Personeli', 'Spa', '20.00', '08.00', 'Pazar-Pazartesi'],
+        [29, null, null, 8984901187, 'Farhan', 'Edwards', '15/2/1953', 67, '18138491711', '232 Maine Avenue Panama City FL 32401', 'Farhan@gmail.com', '0 rh+', 10000, 'Sekreter', 'İdari', '20.00', '08.00', 'Cumartesi-Pazar']
+        
+
+    ]
     
     con.query(sql, [values], function (err, result) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
     });
 
-    sql = "INSERT INTO tesis(tesis_no, aktivite_no, kat, musteri_kapasitesi, personel_kapasitesi, faaliyet_baslangic_saati, faaliyet_bitis_saati, tesis_tipi, aktif_personel_sayisi) VALUES ?";
+
+    sql = "INSERT INTO oda(oda_no, rezervasyon_no, yatak_sayisi, oda_tipi, fiyat, kat, cephe) VALUES ?";
     values = [
-        [1, null, 2, 75, 10, "08:30", "11:30", "Breakfast Saloon", 6],
-        [2, null, 1, 30, 5, "12:30", "18:30", "Game Saloon", 4],
-        [3, null, 0, 100, 10, "08:30", "23:30", "Pool", 6],
-        [4, null, 2, 40, 4, "19:00", "02:00", "Bar", 4],
-        [5, null, 0, 44, 3, "08:30", "22:30", "Football Pitch", 1],
-        [6, null, 2, 20, 10, "14:00", "18:00", "Massage Spa", 9],
-        [7, null, 1, 50, 1, "20:30", "23:30", "Movie Theater", 1],
-        [8, null, 3, 80, 20, "22:30", "03:30", "Disco", 16],
-        [9, null, 0, 30, 5, "09:00", "17:00", "Golf Pitch", 5],
-        [10, null, 3, 100, 30, "18:30", "21:30", "Diner Saloon", 27]
-    ];
+        [0, 4916, 1, 'Normal', 4000, 1, 'Doğu'],
+        [1, 2871, 6, 'Ultra Deluxe', 750, 3, 'Doğu'],
+        [2, 2896, 4, 'Family Room', 15000, 4, 'Kuzey'],
+        [3, 2378, 6, 'Ultra Deluxe', 500, 2, 'Kuzey'],
+        [4, 578, 1, 'Suite', 2000, 3, 'Kuzey'],
+        [5, 4940, 3, 'Deluxe', 15000, 1, 'Güney'],
+        [6, 467, 3, 'Normal', 1000, 1, 'Güney'],
+        [7, 975, 3, 'Ultra Deluxe', 4000, 4, 'Doğu'],
+        [8, 1128, 1, 'Deluxe', 1000, 2, 'Güney'],
+        [9, 1840, 5, 'Ultra Deluxe', 4000, 1, 'Güney'],
+        [10, 3105, 2, 'Normal', 15000, 3, 'Güney'],
+        [11, 3767, 4, 'Normal', 2000, 4, 'Kuzey'],
+        [12, 1721, 5, 'Ultra Deluxe', 1000, 4, 'Güney'],
+        [13, 4782, 1, 'Deluxe', 2000, 2, 'Güney'],
+        [14, 1958, 2, 'Normal', 750, 3, 'Kuzey'],
+        [15, 4299, 5, 'King Suite', 4000, 3, 'Güney'],
+        [16, 4399, 2, 'King Suite', 15000, 5, 'Doğu'],
+        [17, 4060, 2, 'Suite', 15000, 1, 'Kuzey'],
+        [18, 1650, 2, 'King Suite', 2000, 5, 'Batı'],
+        [19, 2306, 1, 'Deluxe', 15000, 4, 'Kuzey'],
+        [20, 1894, 1, 'Normal', 4000, 2, 'Güney'],
+        [21, 487, 5, 'Suite', 4000, 5, 'Kuzey'],
+        [22, 5104, 2, 'Normal', 750, 5, 'Batı'],
+        [23, 3468, 4, 'Deluxe', 2000, 3, 'Batı'],
+        [24, 3098, 6, 'King Suite', 2000, 2, 'Doğu'],
+        [25, 5046, 4, 'Family Room', 1000, 4, 'Doğu'],
+        [26, 1946, 3, 'King Suite', 750, 1, 'Batı'],
+        [27, 688, 2, 'Suite', 4000, 5, 'Kuzey'],
+        [28, 4329, 1, 'Family Room', 750, 3, 'Batı'],
+        [29, 3682, 2, 'Normal', 750, 4, 'Batı'],
+        [30, 5221, 2, 'Deluxe', 500, 3, 'Doğu'],
+        [31, 4444, 3, 'Suite', 750, 4, 'Batı'],
+        [32, 1762, 4, 'Suite', 2000, 5, 'Kuzey'],
+        [33, 423, 1, 'Normal', 1000, 2, 'Kuzey'],
+        [34, 2444, 6, 'King Suite', 15000, 3, 'Doğu'],
+        [35, 1040, 5, 'King Suite', 15000, 4, 'Doğu'],
+        [36, 1642, 2, 'Family Room', 2000, 2, 'Güney'],
+        [37, 3262, 3, 'Deluxe', 2000, 1, 'Batı'],
+        [38, 3200, 2, 'Ultra Deluxe', 1000, 3, 'Güney'],
+        [39, 3791, 1, 'Normal', 4000, 3, 'Kuzey'],
+        [40, 1160, 3, 'Normal', 4000, 4, 'Batı'],
+        [41, 3489, 2, 'Suite', 500, 4, 'Güney'],
+        [42, 1817, 6, 'King Suite', 500, 5, 'Batı'],
+        [43, 3475, 5, 'Ultra Deluxe', 4000, 2, 'Batı'],
+        [44, 4103, 6, 'Family Room', 1000, 4, 'Güney'],
+        [45, 390, 4, 'Ultra Deluxe', 1000, 5, 'Batı'],
+        [46, 4841, 2, 'King Suite', 2000, 1, 'Doğu'],
+        [47, 591, 3, 'Suite', 4000, 3, 'Doğu'],
+        [48, 1656, 6, 'Family Room', 4000, 2, 'Batı'],
+        [49, 4525, 3, 'Family Room', 2000, 1, 'Güney'],
+        [50, 4318, 2, 'Deluxe', 2000, 5, 'Güney'],
+        [51, 3249, 2, 'Deluxe', 500, 1, 'Güney'],
+        [52, 4971, 4, 'Suite', 1000, 1, 'Güney'],
+        [53, 3984, 4, 'Family Room', 1000, 4, 'Batı'],
+        [54, 4120, 3, 'Deluxe', 500, 4, 'Güney'],
+        [55, 728, 3, 'Normal', 15000, 3, 'Batı'],
+        [56, 2380, 1, 'Deluxe', 500, 4, 'Kuzey'],
+        [57, 4337, 3, 'Normal', 500, 5, 'Kuzey'],
+        [58, 2825, 6, 'King Suite', 1000, 1, 'Doğu'],
+        [59, 4040, 5, 'Suite', 750, 1, 'Batı'],
+        [60, 371, 5, 'Ultra Deluxe', 1000, 5, 'Batı'],
+        [61, 4190, 4, 'King Suite', 15000, 2, 'Kuzey'],
+        [62, 2879, 4, 'Suite', 15000, 3, 'Doğu'],
+        [63, 1030, 5, 'Family Room', 15000, 3, 'Kuzey'],
+        [64, 3694, 2, 'Normal', 15000, 4, 'Doğu'],
+        [65, 4538, 1, 'Family Room', 500, 4, 'Güney'],
+        [66, 2053, 5, 'Deluxe', 500, 1, 'Batı'],
+        [67, 4838, 5, 'Normal', 750, 1, 'Batı'],
+        [68, 1226, 1, 'Deluxe', 4000, 3, 'Batı'],
+        [69, 1352, 4, 'Deluxe', 15000, 4, 'Kuzey'],
+        [70, 4435, 3, 'Suite', 15000, 1, 'Kuzey'],
+        [71, 779, 6, 'Normal', 500, 1, 'Batı'],
+        [72, 459, 3, 'King Suite', 4000, 4, 'Batı'],
+        [73, 2072, 5, 'King Suite', 500, 1, 'Doğu'],
+        [74, 4443, 3, 'Normal', 4000, 4, 'Güney']
+        
+    ]
     
     con.query(sql, [values], function (err, result) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
     });
 
-    sql = "INSERT INTO ticari_tesis(mulk_no, kat, kira, tip, vergi_no, kiralayan, sozlesme_baslangic_tarihi, sozlesme_bitis_tarihi) VALUES ?";
+
+    sql = "INSERT INTO aktivite(aktivite_no, musteri_no, aktivite_baslangic_tarihi, aktivite_bitis_tarihi, aktivite_ismi, ucret) VALUES ?";
     values = [
-        [1, 2, 3000, 'Conferance Room', 60069468, "Eco Focus", "2021/04/15", "2021/04/19"],
-        [2, 0, 5000, 'Market', 24655609, "Migros Market", "2017/08/01", "2023/08/01"],
-        [3, 2, 3000, 'Conferance Room', 41925201, "Inspire Fitness Co", "2016/01/22", "2016/01/24"],
-        [4, 0, 8000, 'Market', 21116262, "Migros Market", "2019/03/1", "2025/03/1"],
-        [5, 0, 8000, 'Market', 35309419, "A101", "2021/01/01", "2027/01/01"]
-    ];
+        [0, 47, '28/1/1957', '32/1/1957', 'Dans Gösterisi', 100],
+        [1, null, '26/8/1959', '27/8/1959', 'Dans Gösterisi', 1000],
+        [2, 43, '5/5/1987', '8/5/1987', 'Paintball', 0],
+        [3, 18, '18/2/1997', '21/2/1997', 'Egzotik Masaj', 0],
+        [4, 36, '7/2/1987', '7/2/1987', 'Golf', 50],
+        [5, null, '29/4/1956', '30/4/1956', 'Dans Gösterisi', 500],
+        [6, 2, '30/3/1970', '34/3/1970', 'Diving Yarismasi', 0],
+        [7, 9, '30/8/1982', '31/8/1982', 'Golf', 250],
+        [8, 45, '2/8/2002', '4/8/2002', 'Su Topu', 100],
+        [9, 27, '20/1/1959', '23/1/1959', 'Dans Gösterisi', 1000]
+         
+    ]
     
     con.query(sql, [values], function (err, result) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
     });
+
+
+
+
 });
