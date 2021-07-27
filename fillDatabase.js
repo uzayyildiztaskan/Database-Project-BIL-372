7b9f0ed37fe8a23d822d7e308e9769da49b4ed26
uzayyildiztaskan <55345785+uzayyildiztaskan@users.noreply.github.com>
2021-07-27 14:35:21 +0300

    Added dummy datas to fill tables.
    Previous tables filled: musteri, bireysel_musteri
    Current tables filled : musteri, bireysel_musteri, tesis, ticari_tesis, rezervasyon

--- fillDatabase.js
+++ fillDatabase.js
@@ -103,4 +103,81 @@ con.connect(function(err) {
         if (err) throw err;
         console.log("Number of records inserted: " + result.affectedRows);
     });
+
+    sql = "INSERT INTO rezervasyon(rezervasyon_no, musteri_no, oda_sayisi, kisi_sayisi, baslangic_tarihi, bitis_tarihi, fiyat) VALUES ?";
+    values = [
+        [543, null, 3, 1, '1982/12/2', '1982/12/8', 3690],
+        [532, null, 2, 5, '2007/9/15', '2007/9/18', 2556],
+        [182, null, 3, 4, '2000/3/5', '2000/3/13', 2139],
+        [439, null, 1, 5, '2012/5/3', '2012/5/5', 521],
+        [503, null, 1, 5, '2009/8/16', '2009/8/22', 870],
+        [353, null, 1, 1, '2005/6/11', '2005/6/15', 1441],
+        [433, null, 2, 2, '2004/2/3', '2004/2/8', 2470],
+        [79, null, 3, 3, '2008/7/8', '2008/7/15', 4143],
+        [177, null, 1, 3, '2015/10/13', '2015/10/19', 1160],
+        [53, null, 3, 4, '2018/2/22', '2018/2/25', 3318],
+        [997, null, 3, 5, '1983/10/30', '1983/11/5', 2985],
+        [502, null, 3, 3, '1983/10/12', '1983/10/18', 4272],
+        [782, null, 2, 7, '2013/5/28', '2013/6/3', 1578],
+        [440, null, 1, 6, '1983/12/21', '1983/12/26', 814],
+        [236, null, 1, 6, '2019/5/20', '2019/5/28', 543],
+        [179, null, 3, 4, '2010/1/17', '2010/1/19', 2787],
+        [379, null, 2, 4, '1986/3/8', '1986/3/15', 2852],
+        [381, null, 2, 4, '1995/5/27', '1995/6/1', 2428],
+        [936, null, 2, 3, '2020/11/12', '2020/11/14', 1920],
+        [407, null, 3, 1, '1984/10/10', '1984/10/13', 3801],
+        [278, null, 2, 4, '1980/8/10', '1980/8/13', 2362],
+        [469, null, 2, 7, '1993/12/30', '1994/1/7', 2870],
+        [411, null, 3, 7, '1990/8/15', '1990/8/20', 3876],
+        [89, null, 3, 3, '2009/1/22', '2009/1/29', 3456],
+        [519, null, 1, 3, '1990/12/10', '1990/12/17', 1310],
+        [347, null, 2, 6, '1985/3/30', '1985/3/7', 1020],
+        [553, null, 2, 6, '2010/2/16', '2010/2/20', 2384],
+        [438, null, 2, 2, '2000/2/14', '2000/2/18', 1144],
+        [377, null, 2, 1, '2018/6/27', '2018/7/4', 1912],
+        [27, null, 2, 1, '1993/10/4', '1993/10/7', 1850],
+        [958, null, 1, 2, '1993/11/10', '1993/11/12', 1290],
+        [907, null, 3, 6, '1988/3/7', '1988/3/14', 2892],
+        [115, null, 2, 3, '1985/7/5', '1985/7/7', 2280],
+        [456, null, 2, 4, '1981/11/22', '1981/11/29', 2846],
+        [310, null, 2, 2, '1989/8/11', '1989/8/18', 1608]
+    ];
+    
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+
+    sql = "INSERT INTO tesis(tesis_no, aktivite_no, kat, musteri_kapasitesi, personel_kapasitesi, faaliyet_baslangic_saati, faaliyet_bitis_saati, tesis_tipi, aktif_personel_sayisi) VALUES ?";
+    values = [
+        [1, null, 2, 75, 10, "08:30", "11:30", "Breakfast Saloon", 6],
+        [2, null, 1, 30, 5, "12:30", "18:30", "Game Saloon", 4],
+        [3, null, 0, 100, 10, "08:30", "23:30", "Pool", 6],
+        [4, null, 2, 40, 4, "19:00", "02:00", "Bar", 4],
+        [5, null, 0, 44, 3, "08:30", "22:30", "Football Pitch", 1],
+        [6, null, 2, 20, 10, "14:00", "18:00", "Massage Spa", 9],
+        [7, null, 1, 50, 1, "20:30", "23:30", "Movie Theater", 1],
+        [8, null, 3, 80, 20, "22:30", "03:30", "Disco", 16],
+        [9, null, 0, 30, 5, "09:00", "17:00", "Golf Pitch", 5],
+        [10, null, 3, 100, 30, "18:30", "21:30", "Diner Saloon", 27]
+    ];
+    
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+
+    sql = "INSERT INTO ticari_tesis(mulk_no, kat, kira, tip, vergi_no, kiralayan, sozlesme_baslangic_tarihi, sozlesme_bitis_tarihi) VALUES ?";
+    values = [
+        [1, 2, 3000, 'Conferance Room', 60069468, "Eco Focus", "2021/04/15", "2021/04/19"],
+        [2, 0, 5000, 'Market', 24655609, "Migros Market", "2017/08/01", "2023/08/01"],
+        [3, 2, 3000, 'Conferance Room', 41925201, "Inspire Fitness Co", "2016/01/22", "2016/01/24"],
+        [4, 0, 8000, 'Market', 21116262, "Migros Market", "2019/03/1", "2025/03/1"],
+        [5, 0, 8000, 'Market', 35309419, "A101", "2021/01/01", "2027/01/01"]
+    ];
+    
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
 });
